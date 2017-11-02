e47c6e7a5cccc2506edfc7f8fd706857ee26a0f9
LanceShu <869924650@qq.com>
2017-11-02 18:58:24 +0800

    实现图库功能

---
+++ src/SimpleBox/ImageUrl.java
@@ -0,0 +1,46 @@
+package SimpleBox;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+
+
+@WebServlet(name = "ImageUrl",urlPatterns = "/ImageUrl")
+public class ImageUrl extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        long id = System.currentTimeMillis();
+        ServletInputStream sis = request.getInputStream();
+        String pathUrl = "C:/apache-tomcat-8.5.23/webapps/SimpleBox";
+        String imageUrl = "/picture/" + id + ".jpg";
+        OutputStream os = new FileOutputStream(pathUrl + imageUrl);
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+
+        byte[] bytes = new byte[1024];
+        int data = 0;
+        while((data = sis.read(bytes)) != -1){
+            bos.write(bytes,0,data);
+        }
+        sis.close();
+        bos.close();
+        os.close();
+
+        File file = new File(pathUrl + imageUrl);
+        if(file.exists()){
+            pw.println("success");
+            pw.println("http://192.168.56.1:8080/SimpleBox" + imageUrl);
+        }else{
+            pw.println("failure");
+        }
+
+    }
+}
